e5190b46129a95718149ed181116fc125a224090
jefflee1103 <jefflee1103@gmail.com>
2025-07-31 15:31:02 +0100

    create import_gtf() function

--- src/misc.R
+++ src/misc.R
@@ -2,3 +2,32 @@ str_revcomp <- function(seq){
   # For reverse complementation of character string
   chartr("ATGC", "TACG", seq) %>% stringi::stri_reverse()
 }
+
+#' Import and convert a GTF/GFF file into a valr compatible bed tbl format
+#'
+#' @description This function will output a tibble with the
+#' required chrom, start, and end columns, as well as other columns depending
+#' on content in GTF/GFF file.
+#'
+#' @param path path to gtf or gff file
+#' @param zero_based if TRUE, convert to zero based
+#'
+#' @examples
+#'
+#' gtf <- read_gtf(valr_example("hg19.gencode.gtf.gz"))
+#' head(gtf)
+#'
+#' @importFrom rtracklayer import
+#' @export
+import_gtf <- function(path, zero_based = TRUE) {
+  gtf <- rtracklayer::import(path)
+  gtf <- as.data.frame(gtf)
+  gtf <- dplyr::mutate_if(gtf, is.factor, as.character)
+  res <- dplyr::rename(gtf, chrom = seqnames)
+
+  if (zero_based) {
+    res <- dplyr::mutate(res, start = start - 1L)
+  }
+
+  tibble::as_tibble(res)
+}
